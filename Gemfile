d1d9ba16ba9ce381fe934c219bc149aac81d745b
Court3nay <courtenay@caboo.se>
2011-05-17 19:43:26 -0700

    Use latest rails and bundler

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source :gemcutter
+
+gem 'mysql'
+
+gem 'rack', '1.1.2'
+gem 'rails', '2.3.11'
+
+group :test do
+  gem 'matchy'
+end
+
+gem 'tinder', '>=1.4.0'
+gem 'fastercsv', '1.2.3'
+gem 'googlecharts', '1.3.6'
+gem 'hpricot', '>=0.6'
+gem 'net-toc', '0.2'
--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,11 +19,6 @@ Rails::Initializer.run do |config|
     :secret      => 'SEKRITSEKRITSEKRITSEKRITSEKRITSEKRITSEKRITSEKRITSEKRIT!'
   }
 
-  config.gem 'tinder', :version => '>=1.4.0'
-  config.gem 'fastercsv', :version => '1.2.3'
-  config.gem 'googlecharts', :lib => "gchart", :version => '1.3.6'
-  config.gem 'hpricot', :version => '>=0.6'
-  config.gem 'net-toc', :lib => 'net/toc', :version => '0.2'
   config.active_support.use_standard_json_time_format = true
   config.active_record.include_root_in_json = true
 
